73b3391a607bd8ae8e1d3ee5a791b725a3cdcdb0
ysuga <ysuga@ysuga.net>
2013-11-12 09:06:08 +0900

    update for osx10.9

--- include/LeapRTC/LeapRTC.h
+++ include/LeapRTC/LeapRTC.h
@@ -10,25 +10,26 @@
 #ifndef LEAPRTC_H
 #define LEAPRTC_H
 
+#include <rtm/idl/BasicDataTypeSkel.h>
+#include <rtm/idl/ExtendedDataTypesSkel.h>
+#include <rtm/idl/InterfaceDataTypesSkel.h>
+// Service Consumer stub headers
+// <rtc-template block="consumer_stub_h">
+#include "leapStub.h"
+
+// </rtc-template>
+
 #include <rtm/Manager.h>
 #include <rtm/DataFlowComponentBase.h>
 #include <rtm/CorbaPort.h>
 #include <rtm/DataInPort.h>
 #include <rtm/DataOutPort.h>
-#include <rtm/idl/BasicDataTypeSkel.h>
-#include <rtm/idl/ExtendedDataTypesSkel.h>
-#include <rtm/idl/InterfaceDataTypesSkel.h>
 
 // Service implementation headers
 // <rtc-template block="service_impl_h">
 
 // </rtc-template>
 
-// Service Consumer stub headers
-// <rtc-template block="consumer_stub_h">
-#include "leapStub.h"
-
-// </rtc-template>
 
 #include "LeapListener.h"
 
